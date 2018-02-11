8a2de8ef84a9b9a08e23cba5799b37ca318f975d
Mike Calvanese <mfcalvanese@gmail.com>
2018-02-11 14:25:37 -0500

    add ERC20Basic implementation to ERC20Delegate

--- contracts/Delegates/ERC20Delegate.sol
+++ contracts/Delegates/ERC20Delegate.sol
@@ -1,19 +1,47 @@
 pragma solidity ^0.4.18;
 
+import "zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "../Libraries/ERC20Lib.sol";
 import "../Core/Storage/MasterStorage.sol";
 import "../Core/Storage/StorageConsumer.sol";
 import "../Core/Storage/StorageLib.sol";
 
-contract ERC20Delegate is StorageConsumer {
+contract ERC20Delegate is StorageConsumer, ERC20Basic {
 
   using ERC20Lib for StorageLib.Data;
 
   function ERC20Delegate(MasterStorage store) public StorageConsumer(store) { }
-
+ 
+  /**
+  * @dev total number of tokens in existence
+  */
   function totalSupply() public view returns (uint256) {
     return _data.totalSupply();
+  }
+
+  /**
+  * @dev transfer token for a specified address
+  * @param to The address to transfer to.
+  * @param value The amount to be transferred.
+  */
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(to != address(0));
+    require(value <= _data.getBalance(msg.sender));
+
+    _data.subBalance(msg.sender, value);
+    _data.addBalance(to, value);
+    Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address owner) public view returns (uint256 balance) {
+    return _data.getBalance(owner);
   }
 
 }
--- contracts/Libraries/ERC20Lib.sol
+++ contracts/Libraries/ERC20Lib.sol
@@ -7,13 +7,28 @@ import "../Core/Storage/StorageLib.sol";
 library ERC20Lib {
   using SafeMath for uint256;
 
-  function addSupply(StorageLib.Data storage data, uint256 amount) public {
-    uint256 totalSupply = data.store.getUint("totalSupply");
-    data.store.setUint("totalSupply", totalSupply.add(amount));
+  function getBalance(StorageLib.Data storage data, address balanceHolder) public view returns (uint256) {
+    return data.store.getUint(keccak256("balances", balanceHolder));
   }
 
   function totalSupply(StorageLib.Data storage data) public view returns (uint256) {
     return data.store.getUint("totalSupply");
+  }
+
+  function addSupply(StorageLib.Data storage data, uint256 amount) public {
+    data.store.setUint("totalSupply", totalSupply(data).add(amount));
+  }
+
+  function addBalance(StorageLib.Data storage data, address balanceHolder, uint256 amount) public {
+    setBalance(data, balanceHolder, getBalance(data, balanceHolder).add(amount));
+  }
+
+  function subBalance(StorageLib.Data storage data, address balanceHolder, uint256 amount) public {
+    setBalance(data, balanceHolder, getBalance(data, balanceHolder).sub(amount));
+  }
+
+  function setBalance(StorageLib.Data storage data, address balanceHolder, uint256 amount) private {
+    data.store.setUint(keccak256("balances", balanceHolder), amount);
   }
 
 }
